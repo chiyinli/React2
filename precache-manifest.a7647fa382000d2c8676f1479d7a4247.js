586b9da9f86910e584fb722582f25c8c9ac1d268
Chi Yin Li <chiyinli.mastercyber1@gmail.com>
2020-03-25 08:35:47 +0000

    Updates

---
+++ precache-manifest.a7647fa382000d2c8676f1479d7a4247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77fbd510472df478ad1fb4758ce2bab1",
+    "url": "/activity2/index.html"
+  },
+  {
+    "revision": "d1b166c57c234e5ce148",
+    "url": "/activity2/static/css/main.473ab545.chunk.css"
+  },
+  {
+    "revision": "851c59d93bc8a9223add",
+    "url": "/activity2/static/js/2.efd94d1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/activity2/static/js/2.efd94d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b166c57c234e5ce148",
+    "url": "/activity2/static/js/main.66f1febc.chunk.js"
+  },
+  {
+    "revision": "3226185bc343080fe3eb",
+    "url": "/activity2/static/js/runtime-main.0ddc0810.js"
+  }
+]);
